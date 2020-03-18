edab46a9c9e849e4e5cf4cea5a2a16b41a50dbe3
Nico <n.gaget69@gmail.com>
2020-03-18 14:50:31 +0100

    creation _header.php & _footer.php

---
+++ _header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Fjalla+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <title>Bienvenue sur Escalagones !</title>
+</head>
+
+<body>
+    <header>
+        <div class="btn-navigation">
+            <div class="barre"></div>
+            <div class="barre"></div>
+            <div class="barre"></div>
+            <div class="barre"></div>
+        </div>
+
+        <div class="body-header">
+            <nav class="nav-bar-escalade">
+                <ul>
+                    <li><a href="index.html">Accueil</a></li>
+                    <li><a href="interieur.html">Intérieur</a></li>
+                    <li><a href="exterieur.html">Extérieur</a></li>
+                    <li><a href="grimperEnsemble.html">Grimper ensemble</a></li>
+                    <li><a href="materiel.html">Matériel</a></li>
+                    <li><a href="grimperEnsemble.html#formulaire" title="ouvre un formulaire de contact">Contactez-nous</a></li>
+                </ul>
+            </nav>
+
+        </div>
+
+    </header>
