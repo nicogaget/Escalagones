9edc699f57bc6870d29326b68ab8d7563996a345
Nico <n.gaget69@gmail.com>
2020-03-19 01:39:30 +0100

    iframe setting

--- interieur.php
+++ interieur.php
@@ -58,8 +58,30 @@
                 <a href="<?php echo $room[webSite]?> " target="_blank"><?php  echo '<br>'.$room[webSiteName]?></a>
             </div>
             <div class="lien_google">
+                <a href="#" data-toggle="modal" data-target="#exampleModalCenter"
+                   title="permet de voir les mentions légales du site">plan</a>
+            </div>
 
-                <a href=<?php echo $room[googleMap]?> target="_blank">Google Map</a>
+
+                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
+                     aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Mentions légales</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2785.9383763230508!2d4.904393115565775!3d45.71227307910474!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4c321bdf6db77%3A0xc6605a7208416a22!2sVertical%E2%80%99Art%20Lyon!5e0!3m2!1sen!2sfr!4v1584577544456!5m2!1sen!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-success" data-dismiss="modal">OK</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <?php } ?>
